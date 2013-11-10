5af57339b00ab15ca3ee55cf31e57d1338153d2f
foostan <foostan27@gmail.com>
2013-11-10 15:00:47 +0900

    Add a cookbook 'git'.

--- chef/.chef/knife.rb
+++ chef/.chef/knife.rb
@@ -2,6 +2,12 @@ cookbook_path ["cookbooks", "site-cookbooks"]
 node_path     "nodes"
 role_path     "roles"
 data_bag_path "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+
+maintainer       "foostan"
+maintainer_email "dev@fstn.jp"
+
+cookbook_copyright "foostan"
+cookbook_email     "dev@fstn.jp"
+cookbook_license   "mit"
 
 knife[:berkshelf_path] = "cookbooks"
